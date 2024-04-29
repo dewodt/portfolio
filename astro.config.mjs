1017f25fa12798489f35d949150a9f979f82ade0
Dewantoro Triatmojo <dewantorotriatmojo@gmail.com>
2024-04-29 09:46:04 +0700

    feat: add sitemap, robots tdxt. and manifest.

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import { defineConfig } from "astro/config";
 import { loadEnv } from "vite";
 
 // Use loadEnv to load environment variables
+import sitemap from "@astrojs/sitemap";
 const { PUBLIC_SANITY_STUDIO_PROJECT_ID, PUBLIC_SANITY_STUDIO_DATASET } =
   loadEnv(import.meta.env.MODE, process.cwd(), "");
 
@@ -15,8 +16,10 @@ const dataset = PUBLIC_SANITY_STUDIO_DATASET;
 
 // https://astro.build/config
 export default defineConfig({
-  output: "hybrid", // Hybrid+adapter is required to support embedded Sanity Studio
+  // Hybrid+adapter is required to support embedded Sanity Studio
+  output: "hybrid",
   adapter: vercel(),
+  site: "https://dewodt.com",
   image: {
     remotePatterns: [
       {
@@ -29,12 +32,15 @@ export default defineConfig({
   integrations: [
     react(),
     tailwind(),
+    sitemap(),
     sanity({
       projectId,
       dataset,
       studioBasePath: "/admin",
-      useCdn: false, // Statically build no need cdn
-      apiVersion: "2024-04-19", // Set to date of setup to use the latest API version
+      useCdn: false,
+      // Statically build no need cdn
+      apiVersion: "2024-04-19",
+      // Set to date of setup to use the latest API version
       perspective: "published", // Use the published dataset
     }),
   ],
